17e841fc7c8480cfeddb2ce2d38fc5d4d6886a73
quarkum-0 <122341624+quarkum-0@users.noreply.github.com>
2024-09-15 14:39:10 +0530

    portfolio-link change

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,7 +46,7 @@ export default function Home() {
               </p>
               <div className="mt-2 flex items-center self-start lg:self-center">
                 <a
-                  href="https://bit.ly/shrish_kumar_resume"
+                  href="https://bit.ly/Resume-Shrish-Kumar"
                   target="_blank"
                   className={`flex items-center rounded-lg border-2 border-solid bg-dark p-2.5 px-6 text-lg font-semibold
             capitalize text-light hover:border-dark hover:bg-transparent hover:text-dark 
