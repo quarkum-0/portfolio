62ba653b86b337960983afff4a3ddeb420216526
quarkum-0 <122341624+quarkum-0@users.noreply.github.com>
2025-01-19 00:24:22 +0530

    Update: Changed main image

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import lightBulb from "../../public/images/svgs/miscellaneous_icons_1.svg";
-import profilePic from "../../public/images/profile/main.png";
+import profilePic from "../../public/images/profile/Resume_Pic_Main_bg.png";
 import TransitionEffect from "@/components/TransitionEffect";
 
 
